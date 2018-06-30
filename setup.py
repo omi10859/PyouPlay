97a028dc6d0774bd9d58fa917dec3b91596d842c
omi10859 <omkar108591@gmail.com>
2018-06-30 13:31:58 +0530

    release

--- setup.py
+++ setup.py
@@ -1,35 +1,26 @@
 from setuptools import setup, find_packages
-import PyouPlay.pyouplay
+import PyouPlay.get
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="Pyoutube",
-    version="0.1",
+    name='PyouPlay',
+    version='0.1',
     author="Omkar Yadav",
     author_email="omkar10859@gmail.com",
+    packages=['PyouPlay'],
+    scripts=['PyouPlay/get.py'],
+    url="https://github.com/omi10859/PyouPlay",
     description="This is a simple python package when passed with a search argument returns link to video.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    license='Mozilla Public License Version 2.0',
-    keywords='youtube',
-    url="https://github.com/omi10859/PyouPlay",
-    packages=find_packages),
-classifiers=(
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7  ',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        "Operating System :: OS Independent",
-    ),
-install_requires=[
-    'beautifulsoup4'
+    install_requires=[
+        "bs4>=0.0.1",
+        "beautifulsoup4>=4.6.0",
+        "requests>=2.19.1",
+        "urllib3>=1.23",
+        "certifi>=2018.4.16",
+        "chardet>=3.0.4",
+        "lxml>=4.2.3",
     ],
-include_package_data=True,
-zip_safe=False
 )
