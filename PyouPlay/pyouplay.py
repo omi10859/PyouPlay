b088ad7e020a043a4aad776ae0300d9ad56878cd
omi10859 <omkar108591@gmail.com>
2018-06-30 12:35:53 +0530

    fix

--- PyouPlay/pyouplay.py
+++ PyouPlay/pyouplay.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def PyouPlay(search):
+def toplink(search):
     SearchUrl=str('https://www.youtube.com/results?search_query='+search)
     page=requests.get(SearchUrl)
     soup = BeautifulSoup(page.text, "lxml")
@@ -13,7 +13,13 @@ def PyouPlay(search):
     link=soup.findAll(attrs={'class':'yt-uix-tile-link'})
     links=link[0]
     return (links['href'])
+
+def links(search):
+    SearchUrl=str('https://www.youtube.com/results?search_query='+search)
+    page=requests.get(SearchUrl)
+    soup = BeautifulSoup(page.text, "lxml")
+    print(page)
+    link=soup.findAll(attrs={'class':'yt-uix-tile-link'})
+    links=link[0]
     for vid in link:
         return ('https://www.youtube.com' + vid['href'])
-
-print('lol')
