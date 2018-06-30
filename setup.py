36d56d77447524460a7c2419d4d284da1bbe98a8
omi10859 <omkar108591@gmail.com>
2018-06-30 16:46:40 +0530

    release 0.1.3

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
-from setuptools import setup, find_packages
-import PyouPlay.get
+from distutils.core import setup
+from setuptools import setup
+
 
 setup(
     name='PyouPlay',
-    version='0.1.2',
+    version='0.1.3',
     author="Omkar Yadav",
     author_email="omkar10859@gmail.com",
     packages=['PyouPlay'],
-    scripts=['PyouPlay/get.py'],
-    url="https://github.com/omi10859/PyouPlay",
-    description="This is a simple python package when passed with a search argument returns youtube video link.",
-    long_description="A simple package to get youtube video links just by passing a search argument it can give a top one link and top 20 links.",
     install_requires=[
         "bs4>=0.0.1",
         "beautifulsoup4>=4.6.0",
@@ -18,6 +15,11 @@ setup(
         "urllib3>=1.23",
         "certifi>=2018.4.16",
         "chardet>=3.0.4",
-        "lxml>=4.2.3",
+        "lxml>=4.2.3"
     ],
+    scripts=['PyouPlay/get.py'],
+    url="https://github.com/omi10859/PyouPlay",
+    description="This is a simple python package when passed with a search argument, returns youtube video link.",
+    long_description="A simple package to get youtube video links just by passing a search argument it can give a top one link and top 20 links.",
+
 )
