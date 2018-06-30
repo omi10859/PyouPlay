c9b1350070f5d5276bc6a9b68976e984559f00e9
omi10859 <omkar108591@gmail.com>
2018-06-30 12:38:32 +0530

    fix

--- PyouPlay/get.py
+++ PyouPlay/get.py
@@ -1,6 +1,3 @@
-from __future__ import (absolute_import, division,
-                        print_function, unicode_literals)
-from builtins import *
 from bs4 import BeautifulSoup
 import requests
 
